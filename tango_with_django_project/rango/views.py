a2648984adff84abfcbad0efa8c71d5cf9a59235
bayao <bayao@cisco.com>
2015-06-01 16:29:03 +0800

    charpter8,add loggin loggout

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse ,HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Category,Page
 from rango.forms import CategoryForm,PageForm,User,UserProfile,UserForm,UserProfileForm
+from django.contrib.auth import authenticate, login ,logout
+from django.contrib.auth.decorators import login_required
+
 
 def encode(with_blanke):
     no_blanke=with_blanke.replace('_', ' ')
@@ -102,3 +105,34 @@ def register(request):
             'rango/register.html',
             {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
             RequestContext(request))
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render_to_response('rango/login.html', {}, RequestContext(request))
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+# Use the login_required() decorator to ensure only those logged in can access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/rango/')
