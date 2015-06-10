c0e391b3901b39b8fea8003e5f7b04f6561fa0d2
bayao <bayao@cisco.com>
2015-06-10 17:33:06 +0800

    chapter 10

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,6 +5,7 @@ from rango.models import Category,Page
 from rango.forms import CategoryForm,PageForm,User,UserProfile,UserForm,UserProfileForm
 from django.contrib.auth import authenticate, login ,logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 def encode(with_blanke):
@@ -14,7 +15,7 @@ def encode(with_blanke):
 def decode(no_blanke):
     with_blanke=no_blanke.replace(' ','_')
     return with_blanke
-
+'''
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     pages_list = Page.objects.order_by('-views')[:5]
@@ -23,15 +24,93 @@ def index(request):
         #category.url = category.name.replace(' ', '_')
         category.url =  decode(category.name)
     return render_to_response('rango/index.html', context_dict, RequestContext(request))
+'''
+def index(request):
+    context = RequestContext(request)
+
+    category_list = Category.objects.all()
+    context_dict = {'categories': category_list}
+
+    for category in category_list:
+        category.url = decode(category.name)
+
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
+
+    #### NEW CODE ####
+    # Obtain our Response object early so we can add cookie information.
+    response = render_to_response('rango/index.html', context_dict, context)
+
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, we default to zero and cast that.
+    visits = int(request.COOKIES.get('visits', '0'))
+
+    # Does the cookie last_visit exist?
+    if 'last_visit' in request.COOKIES:
+        # Yes it does! Get the cookie's value.
+        last_visit = request.COOKIES['last_visit']
+        # Cast the value to a Python date/time object.
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        # If it's been more than a day since the last visit...
+        if (datetime.now() - last_visit_time).seconds > 5:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            response.set_cookie('visits', visits+1)
+            response.set_cookie('id', "svan")
+            # ...and update the last visit cookie, too.
+            response.set_cookie('last_visit', datetime.now())
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        response.set_cookie('last_visit', datetime.now())
+
+    # Return response back to the user, updating any cookies that need changed.
+    return response
+    '''
+def index(request):
+    context = RequestContext(request)
+
+    category_list = Category.objects.all()
+    context_dict = {'categories': category_list}
+
+    for category in category_list:
+        category.url = decode(category.name)
+
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
+
+    #### NEW CODE ####
+    if request.session.get('last_visit'):
+        # The session has a value for the last visit
+        last_visit_time = request.session.get('last_visit')
+        visits = request.session.get('visits', 0)
+
+        if (datetime.now() - datetime.strptime(last_visit_time[:-7], "%Y-%m-%d %H:%M:%S")).seconds > 5:
+            request.session['visits'] = visits + 1
+            request.session['last_visit'] = str(datetime.now())
+    else:
+        # The get returns None, and the session does not have a value for the last visit.
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = 1
+    #### END NEW CODE ####
+
+    # Render and return the rendered response back to the user.
+    return render_to_response('rango/index.html', context_dict, context)
+'''
 
 def about(request):
     context_dict={'boldmessage':"Rango says: Here is the about page."}
+    if 'visits' in request.COOKIES:
+        visits = request.COOKIES['visits']
+    context_dict['visits']=visits
     return render_to_response('rango/about.html', context_dict, RequestContext(request))
 
 def category(request, category_name_url):
     #category_name = category_name_url.replace('_', ' ')
     category_name = encode(category_name_url)
     context_dict = {'category_name': category_name}
+    context_dict['category_name_url']=category_name_url
     try:
         category = Category.objects.get(name=category_name)
         pages = Page.objects.filter(category=category)
--- tango_with_django_project/readme.txt
+++ tango_with_django_project/readme.txt
@@ -1,1 +1,2 @@
-2015/06/01 charter8 ,add register,add login ,logout.
+2015/06/01 chapter8 ,add register,add login ,logout.
+2015/06/02 chapter9 ,add base.html
